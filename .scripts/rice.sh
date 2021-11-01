9d1897f2d951310d5e7a9eabbce6e1a130e956b4
Faaris <faaris@Arch.localdomain>
2021-11-01 21:20:14 +0000

    Tweaks

--- .scripts/rice.sh
+++ .scripts/rice.sh
@@ -5,3 +5,9 @@ i3-msg "append_layout $HOME/.config/i3/workspace.json"
 kitty cava & microsoft-edge-stable & kitty $HOME/.scripts/rsclock.sh & $HOME/.scripts/spotify.sh &
 sleep 1
 i3-msg restart
+sleep 1
+xdotool mousemove 1880 875
+sleep 1
+xdotool click 1
+sleep 1
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play
