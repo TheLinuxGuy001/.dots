03c50a3ed246ffd9428e5838facc2769f10154ea
Faaris <faaris@Arch.localdomain>
2021-11-07 14:53:55 +0000

    Tweaks

--- .scripts/rice.sh
+++ .scripts/rice.sh
@@ -2,7 +2,7 @@
 
 i3-msg "workspace 1"
 i3-msg "append_layout $HOME/.config/i3/workspace.json"
-kitty cava & microsoft-edge-stable & kitty $HOME/.scripts/rsclock.sh & $HOME/.scripts/spotify.sh &
+kitty cava & firefox & kitty $HOME/.scripts/rsclock.sh & $HOME/.scripts/spotify.sh &
 sleep 1
 i3-msg restart
 sleep 1
