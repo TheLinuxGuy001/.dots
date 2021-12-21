ea4b41c064a10e0a24ca725146c6f60aa4e76562
Faaris <faaris@ArtOfRunit.localdomain>
2021-12-21 18:55:36 +0000

    echo $comment

--- .scripts/push.sh
+++ .scripts/push.sh
@@ -2,4 +2,4 @@
 
 echo "What comment do you want to add?"
 read comment
-cd ~/dotfiles && pacman -Qqe > packages.list && git add -A && git commit -m '$comment' && git push -u -f origin main 
+cd ~/dotfiles && pacman -Qqe > packages.list && git add -A && git commit -m 'echo $comment' && git push -u -f origin main 
