1814d5227991cdfcef17d12f616468314e28d4a3
faaris <faaris@ArchBTW.ArchBTW>
2022-02-08 22:14:08 +0000

    Changes

--- .scripts/res.sh
+++ .scripts/res.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 xset s off &
 xset -dpms &
-xrandr --output DP-0 --mode 2560x1440 --rate 169.83
+xrandr --output DP-2 --primary --mode 2560x1440 --rate 169.83 --pos 1920x0 --rotate normal --output DP-0 --mode 1920x1080 --pos 0x180 --rotate normal --rate 74.97
