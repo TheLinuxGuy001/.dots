80b72aa2f979c1ed30e281cf10da26635ee42477
faaris <faaris@ArchBTW.ArchBTW>
2022-03-13 13:37:44 +0000

    ($msg)

--- .scripts/push.sh
+++ .scripts/push.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-echo 'Hello, what commit message would you like?'
 read msg
 cd ~/dotfiles
 pacman -Qqet > packages.list
-git add -A && git commit -m '$msg'
+git add -A && git commit -m '($msg)'
 git push -u -f origin main
