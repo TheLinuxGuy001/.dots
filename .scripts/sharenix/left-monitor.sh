8d0a11725cd99cafbe7c82ed4026beeff42efafc
Faaris <faaris@Kappa>
2022-05-10 17:50:46 +0100

    Changes

---
+++ .scripts/sharenix/left-monitor.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# lets the user select a region of the screen and uploads the
+# screenshot using sharenix
+
+sharenixtmp=$(mktemp /tmp/nicememe.XXXXXXXXXXXXXXXXXXX)
+mv "$sharenixtmp" "$sharenixtmp.png"
+sharenixtmp="$sharenixtmp.png"
+
+maim -uB -g 1920x1080+0+180 $sharenixtmp || exit $?
+notify-send -i $sharenixtmp "Screenshot" "Copied to clipboard\!"
+
+# check file size (0b means that gnome-screenshot was cancelled)
+sharenixtmpsize=$(wc -c <"$sharenixtmp")
+
+if [ $sharenixtmpsize != 0 ]; then
+    sharenix -c -q "$@" $sharenixtmp | xclip -i -sel cli
+    exit $?
+fi
+
+
+echo "Screenshot canceled"
+exit 1
